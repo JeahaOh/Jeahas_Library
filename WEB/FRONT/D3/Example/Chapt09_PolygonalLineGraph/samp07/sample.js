af0fc8c182384af8b5a5934607a0bb202a9cd67e
JeahaOh <jeaha1217@gmail.com>
2019-12-19 22:20:35 +0900

    Chart.JS Water Depth Data Chart Prototype

--- WEB/FRONT/D3/Example/Chapt09_PolygonalLineGraph/samp07/sample.js
+++ WEB/FRONT/D3/Example/Chapt09_PolygonalLineGraph/samp07/sample.js
@@ -1,29 +1,39 @@
-let svgWidth = 320;
-let svgHeight = 240;
+let svgWidth = 1000;
+let svgHeight = 500;
 let offsetX = 30;
 let offsetY = 20;
 let scale = 2.0;
+// let margin = svgWidth / (dataSet.length - 1);
 
 let dataSet = [
-    { year : 2004, item1 : 10, item2 : 90, item3 : 50 },
-    { year : 2005, item1 : 43, item2 : 77, item3 : 27 },
-    { year : 2006, item1 : 65, item2 : 55, item3 : 45 },
-    { year : 2007, item1 : 8, item2 : 48, item3 : 58 },
-    { year : 2008, item1 : 64, item2 : 64, item3 : 84 },
-    { year : 2009, item1 : 99, item2 : 90, item3 : 70 },
-    { year : 2010, item1 : 75, item2 : 85, item3 : 45 },
-    { year : 2011, item1 : 22, item2 : 62, item3 : 22 },
-    { year : 2012, item1 : 63, item2 : 13, item3 : 30 },
-    { year : 2013, item1 : 80, item2 : 40, item3 : 90 },
-]
-let margin = svgWidth / (dataSet.length - 1);
+  { id: 'Unit_2004', data : [ 10, 90, 50 ] },
+  { id: 'Unit_2005', data : [ 43, 77, 27 ] },
+  { id: 'Unit_2006', data : [ 65, 55, 45 ] },
+  { id: 'Unit_2007', data : [ 8, 48, 58 ] },
+  { id: 'Unit_2008', data : [ 64, 64, 84 ] },
+  { id: 'Unit_2009', data : [ 99, 90, 70 ] },
+  { id: 'Unit_2010', data : [ 75, 85, 45 ] },
+  { id: 'Unit_2011', data : [ 22, 62, 22 ] },
+  { id: 'Unit_2012', data : [ 63, 13, 30 ] },
+  { id: 'Unit_2013', data : [ 80, 40, 90 ] }
+];
 
-drawGraph( dataSet, 'item1', 'itemA', 'basis' );
-drawGraph( dataSet, 'item2', 'itemB', 'basis' );
-drawGraph( dataSet, 'item3', 'itemC', 'basis' );
-drawScale();
+dataSet.forEach( (item, index, array) => {
+  console.log( item.data )
+  item.data.forEach( (it, idx, array) => {
+    console.log( it, 'item' + idx, 'item' + index, )
+    drawGraph( it, 'item' + idx, 'item' + index, 'basis' );
+  });
+});
+
+
+// drawGraph( dataSet, 'item1', 'itemA', 'basis' );
+// drawGraph( dataSet, 'item2', 'itemB', 'basis' );
+// drawGraph( dataSet, 'item3', 'itemC', 'basis' );
+// drawScale();
 
 function drawGraph( dataSet, itemName, cssClassName, type ) {
+  console.log( dataSet, itemName, cssClassName, type );
   let line = d3.svg.line()
   .x((d, i)=> {
     return offsetX + i * margin;
@@ -39,7 +49,7 @@ function drawGraph( dataSet, itemName, cssClassName, type ) {
   .attr('d', line( dataSet ))
 }
 
-function drawScale() {
+function drawScale( id ) {
   let yScale = d3.scale.linear()
   .domain([0, 100])
   .range([scale * 100, 0])
@@ -55,7 +65,7 @@ function drawScale() {
   )
   
   let xScale = d3.scale.linear()
-  .domain([2004, 2013])
+  .domain([id])
   .range([0, svgWidth])
 
   d3.select('#myGraph')
@@ -69,3 +79,7 @@ function drawScale() {
       .ticks(5)
   )
 }
+dataSet.forEach( (item, index, array) => {
+  console.log( item.data )
+  drawScale(item.id)
+});
