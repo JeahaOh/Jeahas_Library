c25f9c2f35910bd122293918a3fe6a58cc0de28e
JeahaOh <jeaha1217@gmail.com>
2020-01-24 01:43:39 +0900

    TileServer Local Public

--- MapTile/TileServer.js
+++ MapTile/TileServer.js
@@ -1,38 +1,51 @@
 /**
+ * 시작시 3번째 인자로 들어오는 값.
+ * 서버로 작동시 내부망인지 아닌지 확인하기 위함.
+ * true, t, local, l 이면 내부망에서 돌리는 중.
+ */
+let isLocal = process.argv[2];
+if( typeof(isLocal) === 'undefined' ) isLocal = "false";
+switch(isLocal.toLowerCase().trim()){
+   case "t": case "local": case "l": case "-l": isLocal = true; break;
+   case "f": case "public": case "p": isLocal = false; break;
+  default: Boolean(isLocal);
+}
+
+/**
  * 시작시 4번째 인자로 들어오는 값.
  * PORT 번호를 정한다.
  * 지정해 주지 않는다면 3000번 포트로 자동 지정 된다.
  */
-let PORT = process.argv[2];
-if( typeof(PORT) !== 'number' || isNaN(PORT) ) PORT = 3000;
-/**
- * 시작시 4번째 인자로 들어오는 값.
- * 당장은 사용하지 않는 변수.
- * 서버로 작동시 내부망인지 아닌지 확인하기 위함.
- * true이면 내부망에서 돌리는 중.
- */
-let isLocal = process.argv[3];
-if( typeof(isLocal) === 'undefined' || isLocal.toString() !== 'true' ) isLocal = false;
-else isLocal = true;
+let PORT = process.argv[3];
+// console.log( 'parseInt(PORT) : ' + (parseInt(PORT)) + ' ' + PORT );
+PORT = parseInt(PORT);
+if(typeof(PORT) !== 'number') PORT = 3000;
+// console.log( "(typeof(PORT) === 'number') : " + (typeof(PORT) !== 'number') + ' ' + PORT );
+// console.log( '(isNaN(PORT)) : ' + (isNaN(PORT)) + ' ' + PORT );
+if( isNaN(PORT)) PORT = 3000;
 
 const express = require('express');
+const app = express();
 //  CORS 허용을 위한 Middleware
 const cors = require('cors');
-const app = express();
 //  응답으로 보내주는 tiles를 Gzip과 Cached로 보내어 Server의 부하를 막기 위한 설정.
 const staticGzip = require('express-static-gzip');
+//  서버 운영체제 종류에 따라 파일 경로 식별자가 달라지므로 사용.
+const os = require('os');
+const idtfr = ( os.type().toLowerCase().indexOf('windows') < 0 ) ? '/' : '\\';
 //  <---------------- Server
 
 //  -----------------> Scraper
-const FS = require('fs');
-const PATH = require('path');
-const AXIOS = require('axios');
+//  내부망에서 사용할 경우 필요 없는 변수이므로.
+const FS = isLocal ? null : require('fs');
+const PATH = isLocal ? null : require('path');
+const AXIOS = isLocal ? null : require('axios');
 
-const destDir = __dirname + '\\tiles\\';
-const originUrl = 'http://xdworld.vworld.kr:8080/2d/Base/service/';
+const destDir = isLocal ? null : __dirname + idtfr + 'tiles' + idtfr;
+const originUrl = isLocal ? null : 'http://xdworld.vworld.kr:8080/2d/Base/service/';
 //  <----------------- Scraper
 
-// ----> Middleware
+// ----> Middlewares
 
 //  CORS Setting
 app.use(cors({
@@ -70,10 +83,10 @@ app.use( '/tiles', staticGzip( __dirname + '/tiles', {
 );
 
 app.use( '/', express.static( __dirname + '/static') );
-// <---- Middleware
+// <---- Middlewares
 
 
-const imgDownload = async function (url, dest, filename ) {
+const imgDownload = isLocal ? null : async function (url, dest, filename ) {
   console.log( 'START IMG SCRAPE' );
   url = url + filename;
   let res;
@@ -108,20 +121,23 @@ const imgDownload = async function (url, dest, filename ) {
   }
 }
 
-app.get('/tiles/:z/:x/:y', (req, res) => { 
+!isLocal && app.get('/tiles/:z/:x/:y', (req, res) => {
   let params = req.url.replace('/tiles/', '').replace('.png', '').trim().split('/');
   let z = params[0], x = params[1], y = params[2];
   // console.log( '\nreq.url : ' + params + '\n' );
-  console.log( '\n\tz : ' + z + '\n\tx : ' + x + '\n\ty : ' + y );
-  let subDir = z + '\\' + x;
-  let filename = subDir + '\\' + y +'.png';
+  console.log( '\n\n\tz : ' + z + '\n\tx : ' + x + '\n\ty : ' + y );
+  let subDir = z + idtfr + x;
+  // console.log( 'subDir : ' + subDir );
+  let filename = subDir + idtfr + y +'.png';
+  // console.log( 'filename : ' + filename );
 
   try {
     if(!FS.existsSync(destDir + filename) ) {
       //  요청 파일이 없다면 없는 파일이 무엇인지 보여줌.
-      console.log( destDir + filename, FS.existsSync(destDir + filename) );
+      // console.log( destDir + filename, FS.existsSync(destDir + filename) );
 
       //  없는 파일의 부모 경로가 있는지 확인
+      // console.log( destDir + subDir, FS.existsSync( destDir + subDir) );
       if( !FS.existsSync( destDir + subDir) ) {
         // console.log( destDir + subDir );
         //  부모경로가 없다면 부모 경로 생성
@@ -131,7 +147,7 @@ app.get('/tiles/:z/:x/:y', (req, res) => {
       imgDownload( originUrl, destDir, filename )
       .then( (filename) => {
         // console.log( '\tFile Saving Complete : ' + filename );
-        console.log( filename );
+        // console.log( filename );
       })
       .catch( (filename) => {
         console.log( '\t!! SOMETHING CRASHED !! ' + filename + '\n' );
@@ -140,19 +156,25 @@ app.get('/tiles/:z/:x/:y', (req, res) => {
   } catch ( err ) {
     console.log( err );
   } finally {
-    if( FS.existsSync(destDir + filename) ) res.sendFile( destDir + filename );
-    else res.sendStatus(404);
-
-    res.send();
-    res.end();
+    setTimeout( () => {
+      // console.log( 'finally' );
+      // console.log( res );
+      // console.log( destDir + filename );
+      if( FS.existsSync(destDir + filename) ) res.sendFile( destDir + filename );
+      else res.sendStatus(404); res.end();
+  
+      // res.send();
+      res.end();
+    }, 2000);
   }
 });
 
 
-app.listen( 3000, function() {
+app.listen( PORT, function() {
   console.log("\u001b[2J\u001b[0;0H");
   console.log( '=================== Map Tile Scrape And Servicer Start ===================');
-  console.log( 'Service on local : ' + isLocal );
+  console.log( 'Service At ' + ((os.type() == 'Darwin') ? 'Mac OS' : os.type()) );
+  console.log( 'Service On ' + ( isLocal ? 'Local' : 'Public' ) );
   console.log( 'Service Listen : ' + PORT );
   console.log( 'Service Root Dir : ' +  __dirname );
   console.log( '==========================================================================');
