d06b772e530364a08ad96b7a7b959d23d09a30a7
JeJe1217 <jeaha1217@gmail.com>
2020-09-25 10:47:37 +0900

    Update Browser_Detect.js

--- WEB/FRONT/Browser/Browser_Detect/Browser_Detect.js
+++ WEB/FRONT/Browser/Browser_Detect/Browser_Detect.js
@@ -5,6 +5,7 @@ var browser = (function(){
     var test = function(regexp) { return regexp.test(window.navigator.userAgent);}
     switch(true){
       case test(/edge/i): return "edge";
+      case test(/Edg/i): return "edge";
       case test(/opr/i) && (!!window.opr || !!window.opera): return "opera";
       case test(/chrome/i) && !!window.chrome: return "chrome";
       case test(/trident/i) : return "ie";
